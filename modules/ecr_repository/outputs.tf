52bba0ea1e33ce117875b2611e90d7c326d6d1cb
Oded David <oded@coralogix.com>
2018-07-05 07:26:17 +0300

    Fix: output for ecr_repository

--- modules/ecr_repository/outputs.tf
+++ modules/ecr_repository/outputs.tf
@@ -1,20 +1,20 @@
 output "this_ecr_repository_name" {
   description = "The name of the repository"
-  value       = "${aws_ecr_repository.this.name}"
+  value       = "${element(split(",", join(",", aws_ecr_repository.this.name)), 0)}"
 }
 
 output "this_ecr_repository_arn" {
   description = "Full ARN of the repository"
-  value       = "${aws_ecr_repository.this.arn}"
+  value       = "${element(split(",", join(",", aws_ecr_repository.this.arn)), 0)}"
 }
 
 output "this_ecr_repository_registry_id" {
   description = "The registry ID where the repository was created"
-  value       = "${aws_ecr_repository.this.registry_id}"
+  value       = "${element(split(",", join(",", aws_ecr_repository.this.registry_id)), 0)}"
 }
 
 output "this_ecr_repository_repository_url" {
   description = "The URL of the repository (in the form aws_account_id.dkr.ecr.region.amazonaws.com/repositoryName"
-  value       = "${aws_ecr_repository.this.repository_url}"
+  value       = "${element(split(",", join(",", aws_ecr_repository.this.repository_url)), 0)}"
 }
 
